17bf16b12a051096a234082ecaeb756497d5cca3
rizailhamsyah <rizailhamsyah28@gmail.com>
2021-10-24 20:13:53 +0700

    Delete Project

--- application/models/M_notifikasi.php
+++
@@ -1,9 +0,0 @@
-<?php
-defined('BASEPATH') OR exit('No direct script access allowed');
-
-class M_notifikasi extends CI_Model {
-public function save($data){
-        $this->db->insert('lapor_notifikasi',$data);
-    }
-
-}
